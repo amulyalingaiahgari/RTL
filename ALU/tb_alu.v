44718bdeb38f676e9ca2e2cf3184cd1349ba6120
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-18 16:10:10 +0530

    Update tb_alu.v

--- ALU/tb_alu.v
+++ ALU/tb_alu.v
@@ -2,10 +2,11 @@ module tb_ALU_8bit;
     // Inputs
     reg [7:0] A;
     reg [7:0] B;
-    reg [2:0] ALU_Sel;
+    reg [3:0] ALU_Sel;
     // Outputs
     wire [7:0] ALU_Out;
     wire Zero;
+    wire Carryout;
 
     // Instantiate the ALU
     ALU_8bit uut (
@@ -13,33 +14,40 @@ module tb_ALU_8bit;
         .B(B),
         .ALU_Sel(ALU_Sel),
         .ALU_Out(ALU_Out),
-        .Zero(Zero)
+        .Zero(Zero),
+        .Carryout(Carryout)
     );
 
     // Test sequence
     initial begin
-        $display("Time\tA\tB\tSel\tOut\tZero");
-        $monitor("%0dns\t%h\t%h\t%b\t%h\t%b", $time, A, B, ALU_Sel, ALU_Out, Zero);
+        $display("Time\tA\tB\tSel\tCarry\tOut\tZero");
+        $monitor("%0dns\t%h\t%h\t%b\t%b\t%h\t%b", $time, A, B, ALU_Sel, Carryout, ALU_Out, Zero);
 
         A = 8'h0A; B = 8'h05;
         // Test ADD
-        ALU_Sel = 3'b000; 
+        ALU_Sel = 4'b0000; 
         // Test SUB
-        #10 ALU_Sel = 3'b001; 
+        #10 ALU_Sel = 4'b0001; 
         // Test AND
-        #10 ALU_Sel = 3'b010; 
+        #10 ALU_Sel = 4'b0010; 
         // Test OR
-        #10 ALU_Sel = 3'b011; 
+        #10 ALU_Sel = 4'b0011; 
         // Test XOR
-        #10 ALU_Sel = 3'b100;
-        // Test Shift Left
-        #10 ALU_Sel = 3'b101; 
-        // Test Shift Right
-        #10 ALU_Sel = 3'b110;
-        // Test Pass-through
-        #10 ALU_Sel = 3'b111; 
+        #10 ALU_Sel = 4'b0100;
+        // Test NOT
+        #10 ALU_Sel = 4'b0101; 
+        // Test Shift left
+        #10 ALU_Sel = 4'b0110;
+        // Test Shift right
+        #10 ALU_Sel = 4'b0111; 
+        // Test compare equality
+        #10 ALU_Sel = 4'b1000; 
+        // Test compare greater
+        #10 ALU_Sel = 4'b1001; 
+        // Test compare lesser
+        #10 ALU_Sel = 4'b1010; 
         // Test Zero output
-        #10 A = 8'h00; B = 8'h00; ALU_Sel = 3'b000;
+        #10 A = 8'b0; B = 8'b0; ALU_Sel = 4'b0000;
       
        #10  $finish;
     end
