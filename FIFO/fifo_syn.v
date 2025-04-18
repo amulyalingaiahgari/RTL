971ef02c22bba67c938fc03ee5d9624658f419f2
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-18 07:50:34 +0530

    Update fifo_syn.v
    FIFO (First-In, First-Out) Buffer
    
    Task: Design a FIFO buffer with read and write enable signals and a full/empty status.
    
    Concepts: Memory management, pointers, and control logic.

--- FIFO/fifo_syn.v
+++ FIFO/fifo_syn.v
@@ -15,44 +15,44 @@ reg [depth-1:0]fifo_state;
 
 //logic for write and read data
 always @(posedge clk) begin
-if(rst) begin
-wr_ptr <= 0;
-fifo_state <= 0;
-end
-else begin
-if(wr en && (!full)&&(fifo_state[wr_ptr] == 0)) begin
-wr_ptr <= wr_ptr+1;
-fifo_mem[wr_ptr] <= data_in;
-fifo_state[wr_ptr] <= 1;
-end
-else
-wr_ptr <= 0;
-end
+  if(rst) begin
+    wr_ptr <= 0;
+    fifo_state <= 0;
+  end
+  else begin
+    if(wr en && (!full)&&(fifo_state[wr_ptr] == 0)) begin
+      wr_ptr <= wr_ptr+1;
+      fifo_mem[wr_ptr] <= data_in;
+      fifo_state[wr_ptr] <= 1;
+    end
+    else
+    wr_ptr <= 0;
+  end
 end
 always @(posedge clk) begin
-if(rst) begin
-rd_ptr <= 0;
-fifo_state <= 0;
+  if(rst) begin
+    rd_ptr <= 0;
+    fifo_state <= 0;
   end
-else begin
-if (rd en && (!empty) ) begin
-data_out <= fifo_mem[rd_ptr];
-rd_ptr <= rd_ptr+1;
-fifo_state[rd_ptr] <= 0;
-end
-else
-rd_ptr <= 0;
-end
+  else begin
+    if (rd en && (!empty) ) begin
+      data_out <= fifo_mem[rd_ptr];
+      rd_ptr <= rd_ptr+1;
+      fifo_state[rd_ptr] <= 0;
+    end
+    else
+      rd_ptr <= 0;
+  end
 end
 //logic for full and empty
 always @(posedge clk) begin
-if(rst) begin
-full <= 0;
-empty <= 0;
-end
-else begin
-full <= (fifo_state[wr_ptr] == 1);
-empty <= (fifo_state[rd_ptr] == 0);
-end
+  if(rst) begin
+    full <= 0;
+    empty <= 0;
+  end
+  else begin
+    full <= (fifo_state[wr_ptr] == 1);
+    empty <= (fifo_state[rd_ptr] == 0);
+  end
 end
 endmodule
